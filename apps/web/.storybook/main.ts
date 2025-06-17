bd90a61c861a2438967941451d6a714a507a72a6
drod1107 <davidrodrigueznolimit@gmail.com>
2025-06-16 19:23:26 -0500

    bugfix: repairs storybook unable to find documentation

--- apps/web/.storybook/main.ts
+++ apps/web/.storybook/main.ts
@@ -1,23 +1,10 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import { join, dirname } from "path";
-
-function getAbsolutePath(value: string): string {
-  return dirname(require.resolve(join(value, "package.json")));
-}
 
 const config: StorybookConfig = {
-  stories: [
-    "../../docs/**/*.stories.mdx",
-    "../../docs/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
-  addons: [
-    // getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-docs"),
-    getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-vitest"),
-  ],
+  stories: ["../../docs/**/*.mdx"],
+  addons: ["@storybook/addon-docs"],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: "@storybook/react-vite",
     options: {},
   },
   staticDirs: ["../public"],
